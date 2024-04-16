94d8f526682723c1d3b681029d7e1e8551330f8c
Z3ro0o0 <mirinow@doramail.com>
2024-04-16 10:03:44 +0800

    hehe

--- Ace-Clothing/src/utils/fetchFromApi.js
+++
@@ -1,9 +0,0 @@
-let baseUrl = "https://fakestoreapi.com";
-
-async function fetchFromApi(path) {
-  const res = await fetch(`${baseUrl}/${path}`);
-  const data = await res.json();
-  return data;
-}
-
-export default fetchFromApi;
