fb7dfded36db88cfe4b111681a0ed40d0d1c751e
Z3ro0o0 <mirinow@doramail.com>
2024-04-16 10:02:33 +0800

    first commit

---
+++ Ace-Clothing/src/authentication/Login.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Login.css'; // Import the CSS file for styling
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/v1/auth/token/login', {
+                email,
+                password
+            });
+            localStorage.setItem('token', response.data.auth_token);
+            // Redirect to profile page or handle success as needed
+            window.location.href = '/explore/all';
+        } catch (error) {
+            setError('Invalid email or password');
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <h1>Login</h1>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="rounded-input"
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="rounded-input"
+                />
+                <button type="submit" className="rounded-button">Login</button>
+            </form>
+        </div>
+    );
+}
